ea9b0f2bedc67eeafc744901020864765a1a45de
pquinterome <33006816+pquinterome@users.noreply.github.com>
2022-01-14 15:28:12 +0000

    new

---
+++ code2.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+import pandas as pd
+import seaborn as sns
+print('hello')
+
+p0 = pd.read_csv('HL_Prostate_3Gy_2arc.csv') 
+fig =sns.scatterplot(data=p0, x='MU', x_jitter=True, y='2_1', color='r', edgecolor='black', linewidth=0.9, alpha=0.6, label='2.7Gy')
+fig.savefig("output/image1.png")
