edd5949bfb5b1927d1fc33ac0bb6ecd168ace860
pquinterome <33006816+pquinterome@users.noreply.github.com>
2022-01-16 19:34:56 +0000

    .

--- code2.py
+++ code2.py
@@ -3,10 +3,10 @@ import pandas as pd
 import seaborn as sns
 import os
 
-os.makedirs("output")
+#os.makedirs("output")
 
 print('hello')
 
 p0 = pd.read_csv('HL_Prostate_3Gy_2arc.csv') 
-fig =sns.scatterplot(data=p0, x='MU', x_jitter=True, y='2_1', color='r', edgecolor='black', linewidth=0.9, alpha=0.6, label='2.7Gy')
+fig =sns.scatterplot(data=p0, x='MU', x_jitter=True, y='2_1', color='r', edgecolor='black', linewidth=0.9, alpha=0.6, label='2.7Gy').get_figure()
 fig.savefig("output/image1.png")
