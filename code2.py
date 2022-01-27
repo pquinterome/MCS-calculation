640aca09fa540603d0628ba04752ae6784d23b31
pquinterome <33006816+pquinterome@users.noreply.github.com>
2022-01-27 11:13:22 +0000

    cc

--- code2.py
+++ code2.py
@@ -7,6 +7,6 @@ import os
 
 print('hello')
 
-p0 = pd.read_csv('HL_Prostate_3Gy_2arc.csv') 
+p0 = pd.read_csv('HL_Prostate_3Gy.csv') 
 fig =sns.scatterplot(data=p0, x='MU', x_jitter=True, y='2_1', color='r', edgecolor='black', linewidth=0.9, alpha=0.6, label='2.7Gy').get_figure()
 fig.savefig("output/image1.png")
