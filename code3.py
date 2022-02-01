d3c4b23a3e71093e9ab7ffc45722716746e6cc72
pquinterome <33006816+pquinterome@users.noreply.github.com>
2022-02-01 17:42:19 +0000

    oo

--- code3.py
+++ code3.py
@@ -83,7 +83,7 @@ for train, test in kfold.split(X, y):
     early_stop = EarlyStopping(monitor='val_loss', patience=3)
     batch_size = 3
     
-    r = model.fit(train_generator, epochs=3, validation_data=(X[test], y[test]), callbacks=[early_stop], verbose=0)
+    r = model.fit(train_generator, epochs=300, validation_data=(X[test], y[test]), callbacks=[early_stop], verbose=0)
 
     metrics = pd.DataFrame(model.history.history)
     
