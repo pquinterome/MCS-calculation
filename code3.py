7d6a80b57f874f14898493cce0b0d4fa781c37b0
pquinterome <33006816+pquinterome@users.noreply.github.com>
2022-02-22 11:28:41 +0000

    aswq

--- code3.py
+++ code3.py
@@ -75,11 +75,12 @@ y_val = pd.read_csv('id_val.csv')
 y_val ['2_2'] = y_val ['2_2'].fillna(y_val ['2_2'].mean())
 y_val = np.array(y_val['2_2']/100)
 mu= asarray([['pass'] if x >= 0.98 else ['fail'] for x in y_val])
-y_val = encoder.fit_transform(mu)
+y_val = mu
+#y_val = encoder.fit_transform(mu)
 
 y = np.load('y.npy')
-mu= asarray([['pass'] if x >= 0.98 else ['fail'] for x in y])
-y = encoder.fit_transform(mu)
+#mu= asarray([['pass'] if x >= 0.98 else ['fail'] for x in y])
+#y = encoder.fit_transform(mu)
 
 #print('dataset', ltm.shape)
 #print('labels', y.shape)
@@ -99,7 +100,7 @@ x = Conv2D(filters=32, kernel_size=(4,3), activation='relu', padding='same')(i)
 x = MaxPool2D(pool_size=(2,2))(x)
 x = Flatten()(x)
 x = Dense(180, activation='relu')(x)
-x = Dense(2, activation='softmax')(x)
+x = Dense(1, activation='sigmoid')(x)
 model1 = Model(i, x)
 #Model->2
 i = Input(shape=(112,177,1))
@@ -107,7 +108,7 @@ x = Conv2D(filters=64, kernel_size=(4,3), activation='relu', padding='same')(i)
 x = MaxPool2D(pool_size=(2,2))(x)
 x = Flatten()(x)
 x = Dense(180, activation='relu')(x)
-x = Dense(2, activation='softmax')(x)
+x = Dense(1, activation='sigmoid')(x)
 model2 = Model(i, x)
 #Model->3
 i = Input(shape=(112,177,1))
@@ -115,7 +116,7 @@ x = Conv2D(filters=128, kernel_size=(4,3), activation='relu', padding='same')(i)
 x = MaxPool2D(pool_size=(2,2))(x)
 x = Flatten()(x)
 x = Dense(180, activation='relu')(x)
-x = Dense(2, activation='softmax')(x)
+x = Dense(1, activation='sigmoid')(x)
 model3 = Model(i, x)
 #Model->4
 i = Input(shape=(112,177,1))
@@ -123,7 +124,7 @@ x = Conv2D(filters=128, kernel_size=(4,3), activation='relu', padding='same')(i)
 x = MaxPool2D(pool_size=(2,2))(x)
 x = Flatten()(x)
 x = Dense(180, activation='relu')(x)
-x = Dense(2, activation='softmax')(x)
+x = Dense(1, activation='sigmoid')(x)
 model4 = Model(i, x)
 #Model->5
 i = Input(shape=(112,177,1))
@@ -132,7 +133,7 @@ x = Conv2D(filters=32, kernel_size=(3,3), activation='relu', padding='same')(x)
 x = MaxPool2D(pool_size=(2,2))(x)
 x = Flatten()(x)
 x = Dense(180, activation='relu')(x)
-x = Dense(2, activation='softmax')(x)
+x = Dense(1, activation='sigmoid')(x)
 model5 = Model(i, x)
 
 
@@ -154,18 +155,18 @@ early_stop = EarlyStopping(monitor='val_loss', patience=5)
 models = [model1, model2, model3, model4, model5]
 i = 1
 for model in models:
-    model.compile(loss="categorical_crossentropy", optimizer= "adam", metrics=['accuracy'])
+    model.compile(loss="binary_crossentropy", optimizer= "adam", metrics=['accuracy'])
     #categorical_crossentropy
     #binary_crossentropy
     r = model.fit(x=X_train, y= y_train, validation_data= (X_test, y_test), epochs=100, verbose=0, callbacks=[])
     metrics = pd.DataFrame(model.history.history)
     pred = model.predict(X_test)
-    predictions = np.round(pred)
-    accuracy = accuracy_score(y_test, predictions)
-    #fpr, tpr, thresholds = roc_curve(y_test, pred)
-    #roc_auc = auc(fpr, tpr)  
+    #predictions = np.round(pred)
+    #accuracy = accuracy_score(y_test, predictions)
+    fpr, tpr, thresholds = roc_curve(y_test, pred)
+    roc_auc = auc(fpr, tpr)  
     
-    print(f'AUC_model{i}', accuracy)
+    print(f'AUC_model{i}', roc_auc)
 
     plt.figure(i*i)
     plt.title('Loss [rmse]')
@@ -181,14 +182,14 @@ for model in models:
     plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
     plt.savefig(f'output/acc{i}.png', bbox_inches='tight')
 
-    #plt.figure(15)
-    #plt.title("Receiver operating characteristic example")
-    #plt.plot([0, 1], [0, 1], linestyle='--', lw=2, color='r', label='Chance', alpha=.2)
-    #plt.plot(fpr, tpr, lw=2, alpha=0.3, label=(f'ROC{i}', roc_auc))
-    #plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-    #plt.xlim(-0.05, 1.05)
-    #plt.ylim(-0.05, 1.05)
-    #plt.savefig('output/auc.png', bbox_inches='tight')
+    plt.figure(15)
+    plt.title("Receiver operating characteristic example")
+    plt.plot([0, 1], [0, 1], linestyle='--', lw=2, color='r', label='Chance', alpha=.2)
+    plt.plot(fpr, tpr, lw=2, alpha=0.3, label=(f'ROC{i}', roc_auc))
+    plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    plt.xlim(-0.05, 1.05)
+    plt.ylim(-0.05, 1.05)
+    plt.savefig('output/auc.png', bbox_inches='tight')
 
     i = i+1
 
