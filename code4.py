e636f4dae05faa65e9fb97a38b7d10816e7430e5
pquinterome <33006816+pquinterome@users.noreply.github.com>
2022-03-21 14:23:38 +0000

    asde

--- code4.py
+++ code4.py
@@ -267,7 +267,7 @@ for train, test in kfold.split(X, y):
     #reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=3, min_lr=0.01)
     early_stop = EarlyStopping(monitor='val_loss', patience=5)
     model1.compile(loss="binary_crossentropy", optimizer= 'adam', metrics=['accuracy', roc])
-    model1.fit(train_generator, validation_data= test_generator ,epochs=600, batch_size=5, verbose=0, callbacks=[early_stop, reduce_lr])
+    model1.fit(train_generator, validation_data= test_generator ,epochs=600, batch_size=5, verbose=0, callbacks=[ reduce_lr])
     #metrics = pd.DataFrame(model.history.history)
     #metrics.plot()  
 #-  evaluate the model  
