d41cfa5300f79cc447fc3ecf3052599e5627a304
pquinterome <33006816+pquinterome@users.noreply.github.com>
2022-05-04 14:57:45 +0100

    asdew

--- code3.py
+++ code3.py
@@ -177,7 +177,138 @@ print('X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X')
 #    plt.ylim(-0.05, 1.05)
 #    plt.savefig('output/auc.png', bbox_inches='tight')
 #
-#    i = i+1
+#    i = i+1#########################
+#########################
+seed =18
+np.random.seed(seed)
+tprs1 = []
+aucs1 = []
+fprs1 = []
+mean_fpr = np.linspace(0, 1, 100)
+i = 1
+fig1, ax1 = plt.subplots()
+kfold = StratifiedKFold(n_splits=5, shuffle=True) #, random_state=seed)
+# for i, (train, test) in enumerate(cv.split(X_13 , target)):
+X = ltm.reshape(1231, 70, 177,1)
+for train, test in kfold.split(X, y):
+    #!rm -rf ./logs/
+    data_generator = ImageDataGenerator(horizontal_flip=True, vertical_flip=True)
+    train_generator = data_generator.flow(X[train], y[train], batch_size=5)
+    test_generator = data_generator.flow(X[test], y[test], shuffle=False)
+#-  create model
+    i1 = Input(shape=(70,177,1))
+    x = Conv2D(filters=64, kernel_size=(3,3), activation='relu', padding='same')(i1)
+    x = MaxPool2D(pool_size=(2,2))(x)
+    x = Conv2D(filters=64, kernel_size=(3,3), activation='relu', padding='same')(x)
+    x = MaxPool2D(pool_size=(2,2))(x)
+    x = Dropout(0)(x)
+    x = Flatten()(x)
+    x = Dense(90, activation='relu')(x)
+    x = Dense(1, activation='sigmoid')(x)
+    model1 = Model(i1, x)
+    #model1.summary()
+##- compile model    
+    roc = tf.keras.metrics.AUC(name='roc')
+    #adam= tf.keras.optimizers.Adam(learning_rate=0.0005, name='adam')
+    reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=3, min_lr=0.01)
+    early_stop = EarlyStopping(monitor='val_loss', patience=5)
+    model1.compile(loss="binary_crossentropy", optimizer= 'adam', metrics=['accuracy', roc])
+    model1.fit(x=X[train], y= y[train], validation_data=(X[test], y[test]) ,epochs=400, batch_size=5, verbose=0, callbacks=[early_stop, reduce_lr])
+    #metrics = pd.DataFrame(model.history.history)
+    #metrics.plot()  
+#-  evaluate the model  
+    y_pred_keras = model1.predict(X[test]).ravel() 
+    fpr, tpr, thresholds = roc_curve(y[test], y_pred_keras)
+    #fprs1.append(fpr)
+    tprs1.append(interp(mean_fpr, fpr, tpr))
+    roc_auc = auc(fpr, tpr)
+    #roc_auc5 = metrics.auc(fpr, tpr)
+    aucs1.append(roc_auc)
+    #plt.plot(fpr, tpr, lw=2, alpha=0.3, label='ROC fold %d (AUC = %0.2f)' % (i, roc_auc))
+    i= i+1
+ax1.plot([0, 1], [0, 1], linestyle='--', lw=2, color='r', label='Chance', alpha=.8)
+mean_tpr = np.mean(tprs1, axis=0)
+mean_tpr[-1] = 1.0
+mean_auc = np.mean(aucs1)
+std_auc = np.std(aucs1)
+ax1.plot(mean_fpr, mean_tpr, color='b',label=r'Mean ROC (AUC = %0.2f $\pm$ %0.2f)' % (mean_auc, std_auc), lw=2, alpha=.8)
+std_tpr = np.std(tprs1, axis=0)
+tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
+tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
+ax1.fill_between(mean_fpr, tprs_lower, tprs_upper, color='grey', alpha=.2, label=r'$\pm$ 1 std. dev.')
+ax1.set(xlim=[-0.05, 1.05], ylim=[-0.05, 1.05], title="Receiver operating characteristic LTM")
+ax1.legend(loc="right", bbox_to_anchor=(1.65, 0.5))
+plt.ylabel('True Positive Rate')
+plt.xlabel('False Positive Rate')
+plt.savefig('output/drop_00.png', bbox_inches='tight')
+##############################################
+print('LTM model done')
+##############################################
+seed =18
+np.random.seed(seed)
+tprs1 = []
+aucs1 = []
+fprs1 = []
+mean_fpr = np.linspace(0, 1, 100)
+i = 1
+fig1, ax1 = plt.subplots()
+kfold = StratifiedKFold(n_splits=5, shuffle=True) #, random_state=seed)
+# for i, (train, test) in enumerate(cv.split(X_13 , target)):
+X = ltm.reshape(1231, 70, 177,1)
+for train, test in kfold.split(X, y):
+    #!rm -rf ./logs/
+    data_generator = ImageDataGenerator(horizontal_flip=True, vertical_flip=True)
+    train_generator = data_generator.flow(X[train], y[train], batch_size=5)
+    test_generator = data_generator.flow(X[test], y[test], shuffle=False)
+#-  create model
+    i1 = Input(shape=(70,177,1))
+    x = Conv2D(filters=64, kernel_size=(3,3), activation='relu', padding='same')(i1)
+    x = MaxPool2D(pool_size=(2,2))(x)
+    x = Conv2D(filters=64, kernel_size=(3,3), activation='relu', padding='same')(x)
+    x = MaxPool2D(pool_size=(2,2))(x)
+    x = Dropout(0.1)(x)
+    x = Flatten()(x)
+    x = Dense(90, activation='relu')(x)
+    x = Dense(1, activation='sigmoid')(x)
+    model1 = Model(i1, x)
+    #model1.summary()
+##- compile model    
+    roc = tf.keras.metrics.AUC(name='roc')
+    #adam= tf.keras.optimizers.Adam(learning_rate=0.0005, name='adam')
+    reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=3, min_lr=0.01)
+    early_stop = EarlyStopping(monitor='val_loss', patience=5)
+    model1.compile(loss="binary_crossentropy", optimizer= 'adam', metrics=['accuracy', roc])
+    model1.fit(x=X[train], y= y[train], validation_data=(X[test], y[test]) ,epochs=400, batch_size=5, verbose=0, callbacks=[early_stop, reduce_lr])
+    #metrics = pd.DataFrame(model.history.history)
+    #metrics.plot()  
+#-  evaluate the model  
+    y_pred_keras = model1.predict(X[test]).ravel() 
+    fpr, tpr, thresholds = roc_curve(y[test], y_pred_keras)
+    #fprs1.append(fpr)
+    tprs1.append(interp(mean_fpr, fpr, tpr))
+    roc_auc = auc(fpr, tpr)
+    #roc_auc5 = metrics.auc(fpr, tpr)
+    aucs1.append(roc_auc)
+    #plt.plot(fpr, tpr, lw=2, alpha=0.3, label='ROC fold %d (AUC = %0.2f)' % (i, roc_auc))
+    i= i+1
+ax1.plot([0, 1], [0, 1], linestyle='--', lw=2, color='r', label='Chance', alpha=.8)
+mean_tpr = np.mean(tprs1, axis=0)
+mean_tpr[-1] = 1.0
+mean_auc = np.mean(aucs1)
+std_auc = np.std(aucs1)
+ax1.plot(mean_fpr, mean_tpr, color='b',label=r'Mean ROC (AUC = %0.2f $\pm$ %0.2f)' % (mean_auc, std_auc), lw=2, alpha=.8)
+std_tpr = np.std(tprs1, axis=0)
+tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
+tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
+ax1.fill_between(mean_fpr, tprs_lower, tprs_upper, color='grey', alpha=.2, label=r'$\pm$ 1 std. dev.')
+ax1.set(xlim=[-0.05, 1.05], ylim=[-0.05, 1.05], title="Receiver operating characteristic LTM")
+ax1.legend(loc="right", bbox_to_anchor=(1.65, 0.5))
+plt.ylabel('True Positive Rate')
+plt.xlabel('False Positive Rate')
+plt.savefig('output/drop_01.png', bbox_inches='tight')
+##############################################
+print('LTM model done')
+##############################################
 #########################
 seed =18
 np.random.seed(seed)
