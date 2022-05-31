572f4d5dec3b73614d240a05f1cf7da167d6eeb3
pquinterome <33006816+pquinterome@users.noreply.github.com>
2022-05-31 07:11:38 -0400

    iytt

--- code3.py
+++ code3.py
@@ -96,27 +96,31 @@ x = MaxPool2D(pool_size=(2,2))(x)
 x = BatchNormalization()(x)
 x = Flatten()(x)
 x = Dense(128, activation='relu')(x)
-x1 = Dense(1, activation=activation)(x)
-model1 = Model(i, x1)
-#model2 = Model(i, x2)
+x = Dense(1, activation=activation)(x)
+model1 = Model(i, x)
 ##Model->2
-#x = Conv2D(filters=64, kernel_size=(7,7), activation='relu', padding='same')(i)
-#x = MaxPool2D(pool_size=(3,3))(x)
-#x = Conv2D(filters=64, kernel_size=(3,3), activation='relu', padding='same')(x)
-#x = MaxPool2D(pool_size=(2,2))(x)
-#x = Flatten()(x)
-#x = Dense(90, activation='relu')(x)
-#x = Dense(1, activation=activation)(x)
-#model2 = Model(i, x)
+x = Conv2D(filters=64, kernel_size=(5,5), activation='relu', padding='same')(i)
+x = Conv2D(filters=64, kernel_size=(5,5), activation='relu', padding='same')(x)
+x = MaxPool2D(pool_size=(3,3))(x)
+x = Dropout(0.2)(x)
+x = Conv2D(filters=64, kernel_size=(3,3), activation='relu', padding='same')(x)
+x = Conv2D(filters=64, kernel_size=(3,3), activation='relu', padding='same')(x)
+x = MaxPool2D(pool_size=(2,2))(x)
+x = Dropout(0.2)(x)
+x = Flatten()(x)
+x = Dense(180, activation='relu')(x)
+x = Dropout(0.2)(x)
+x = Dense(1, activation=activation)(x)
+model2 = Model(i, x)
 ###Model->3
-#x = Conv2D(filters=64, kernel_size=(7,7 ), activation='relu', padding='same')(i)
-#x = MaxPool2D(pool_size=(3,3))(x)
-#x = Conv2D(filters=32, kernel_size=(3,3), activation='relu', padding='same')(x)
-#x = MaxPool2D(pool_size=(2,2))(x)
-#x = Flatten()(x)
-##x = Dense(90, activation='relu')(x)
-#x = Dense(1, activation=activation)(x)
-#model3 = Model(i, x)
+x = Conv2D(filters=64, kernel_size=(7,7 ), activation='relu', padding='same')(i)
+x = MaxPool2D(pool_size=(3,3))(x)
+x = Conv2D(filters=32, kernel_size=(3,3), activation='relu', padding='same')(x)
+x = MaxPool2D(pool_size=(2,2))(x)
+x = Flatten()(x)
+x = Dense(90, activation='relu')(x)
+x = Dense(1, activation=activation)(x)
+model3 = Model(i, x)
 
 
 # %%
@@ -136,243 +140,50 @@ model1 = Model(i, x1)
 early_stop = EarlyStopping(monitor='val_loss', patience=5)
 reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.4, patience=10, min_lr=0.00001)
 
-#models = [model1, model2, model3]
+models = [model1, model2, model3]
 
-##print('all ok')
+print('all ok')
 
-#i = 1
-#for model in models:
-#    model.compile(loss="binary_crossentropy", optimizer= "adam", metrics=['accuracy'])
-#    #categorical_crossentropy
-#    #binary_crossentropy
-#    r = model.fit(x=X_train3, y= y_train, validation_data= (X_test3, y_test), epochs=200, batch_size=10 ,verbose=0, callbacks=[early_stop, reduce_lr])
-#    #r = model.fit(train_generator, validation_data= test_generator, callbacks=[early_stop], epochs=100, verbose=0)
-#    metrics = pd.DataFrame(model.history.history)
-#    pred = model.predict(X_test3)
-#    predictions = np.round(pred)
-#    fpr, tpr, thresholds = roc_curve(y_test, pred)
-#    roc_auc = auc(fpr, tpr)
-#    classes=[0,1]
-#    con_mat = tf.math.confusion_matrix(labels=y_test, predictions=predictions).numpy()
-#    
-#    print(f'AUC_model{i}',  roc_auc)
-#    print(f'Accuracy{i}',   accuracy_score(y_test, predictions))
-#    print(f'precision{i}',  precision_score(y_test, predictions))
-#    print(f'recall{i}',     recall_score(y_test, predictions))
-#    print(f'f1{i}',         f1_score(y_test, predictions))#
-#
-#    plt.figure(i*i)
-#    plt.title('Loss')
-#    plt.plot(metrics[['loss', 'val_loss']], label=[f'loss{i}', f'val_loss{i}'])
-#    #plt.ylim(-0.1, 2)
-#    plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-#    plt.savefig(f'output/loss{i}.png', bbox_inches='tight')#
-#
-#    plt.figure(i*i+1)
-#    plt.title('Accuracy')
-#    plt.plot(metrics[['accuracy', 'val_accuracy']], label=[f'acc{i}', f'val_acc{i}'])
-#    #plt.ylim(0.4, 1.1)
-#    plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-#    plt.savefig(f'output/acc{i}.png', bbox_inches='tight')##
-#
-#    plt.figure(15)
-#    plt.title("Receiver operating characteristic example")
-#    plt.plot([0, 1], [0, 1], linestyle='--', lw=2, color='r', label='Chance', alpha=.2)
-#    plt.plot(fpr, tpr, lw=2, alpha=0.3, label=(f'ROC{i}', roc_auc))
-#    plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-#    #plt.xlim(-0.05, 1.05)
-#    #plt.ylim(-0.05, 1.05)
-#    plt.savefig('output/auc.png', bbox_inches='tight')
-    
-#    i = i+1#########################
-#########################
-seed =18
-np.random.seed(seed)
-tprs1 = []
-aucs1 = []
-fprs1 = []
-mean_fpr = np.linspace(0, 1, 100)
 i = 1
-fig1, ax1 = plt.subplots()
-kfold = StratifiedKFold(n_splits=5, shuffle=True) #, random_state=seed)
-# for i, (train, test) in enumerate(cv.split(X_13 , target)):
-#X = ltm.reshape(1231, 70, 177,1)
-#X = mu.reshape(1231, 176,1)
-X = p.reshape(1231, 512, 512, 1)
-for train, test in kfold.split(X, y):
-    #!rm -rf ./logs/
-    #data_generator = ImageD co    ataGenerator(horizontal_flip=True, vertical_flip=True)
-    #train_generator = data_generator.flow(X[train], y[train], batch_size=5)
-    #test_generator = data_generator.flow(X[test], y[test], shuffle=False)
-#-  create model
-    #i1 = Input(shape=(176,1))
-    #x = Conv1D(filters=90, kernel_size=(5), activation='relu', padding='same')(i1)
-    #x = MaxPool1D(pool_size=(3))(x)
-    ##x = Dropout(rate=0.1)(x)
-    #x = Flatten()(x)
-    #x = BatchNormalization()(x)
-    #x = Dense(90, activation='relu')(x)
-    #x = Dense(1, activation='sigmoid')(x)
-    #x2 = Dense(1, activation='linear')(x)
-    #model1 = Model(i1, x)
-    #model2 = Model(i1, x2)
-    
-    #model1.summary()
-##- compile model    
-    roc = tf.keras.metrics.AUC(name='roc')
-    #adam= tf.keras.optimizers.Adam(learning_rate=0.0005, name='adam')
-    reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=3, min_lr=0.01)
-    early_stop = EarlyStopping(monitor='val_loss', patience=5)
-    model1.compile(loss="binary_crossentropy", optimizer= 'adam', metrics=['accuracy', roc])
-    model1.fit(x=X[train], y= y[train], validation_data=(X[test], y[test]) ,epochs=400, verbose=0, callbacks=[early_stop, reduce_lr])
-    #metrics = pd.DataFrame(model.history.history)
-    #metrics.plot()  
-#-  evaluate the model  
-    y_pred_keras = model1.predict(X[test]).ravel() 
-    fpr, tpr, thresholds = roc_curve(y[test], y_pred_keras)
-    #fprs1.append(fpr)
-    tprs1.append(interp(mean_fpr, fpr, tpr))
+for model in models:
+    model.compile(loss="binary_crossentropy", optimizer= "adam", metrics=['accuracy'])
+    r = model.fit(x=X_train3, y= y_train, validation_data= (X_test3, y_test), epochs=200, batch_size=10 ,verbose=0, callbacks=[early_stop, reduce_lr])
+    #r = model.fit(train_generator, validation_data= test_generator, callbacks=[early_stop], epochs=100, verbose=0)
+    metrics = pd.DataFrame(model.history.history)
+    pred = model.predict(X_test3)
+    predictions = np.round(pred)
+    fpr, tpr, thresholds = roc_curve(y_test, pred)
     roc_auc = auc(fpr, tpr)
-    #roc_auc5 = metrics.auc(fpr, tpr)
-    aucs1.append(roc_auc)
-    #plt.plot(fpr, tpr, lw=2, alpha=0.3, label='ROC fold %d (AUC = %0.2f)' % (i, roc_auc))
+    classes=[0,1]
+    con_mat = tf.math.confusion_matrix(labels=y_test, predictions=predictions).numpy()
     
-    i= i+1
+    print(f'AUC_model{i}',  roc_auc)
+    print(f'Accuracy{i}',   accuracy_score(y_test, predictions))
+    print(f'precision{i}',  precision_score(y_test, predictions))
+    print(f'recall{i}',     recall_score(y_test, predictions))
+    print(f'f1{i}',         f1_score(y_test, predictions))#
 
-ax1.plot([0, 1], [0, 1], linestyle='--', lw=2, color='r', label='Chance', alpha=.8)
-mean_tpr = np.mean(tprs1, axis=0)
-mean_tpr[-1] = 1.0
-mean_auc = np.mean(aucs1)
-std_auc = np.std(aucs1)
-ax1.plot(mean_fpr, mean_tpr, color='b',label=r'Mean ROC (AUC = %0.2f $\pm$ %0.2f)' % (mean_auc, std_auc), lw=2, alpha=.8)
-std_tpr = np.std(tprs1, axis=0)
-tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
-tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
-ax1.fill_between(mean_fpr, tprs_lower, tprs_upper, color='grey', alpha=.2, label=r'$\pm$ 1 std. dev.')
-ax1.set(xlim=[-0.05, 1.05], ylim=[-0.05, 1.05], title="Receiver operating characteristic LTM")
-ax1.legend(loc="right", bbox_to_anchor=(1.65, 0.5))
-#plt.ylabel('True Positive Rate')
-plt.xlabel('False Positive Rate')
-plt.savefig('output/drop_00.png', bbox_inches='tight')
-i=0
-pred = model1.predict(X_test3)
-predictions = np.round(pred)
-print(f'Accuracy{i}',   accuracy_score(y_test, predictions))
-print(f'precision{i}',  precision_score(y_test, predictions))
-print(f'recall{i}',     recall_score(y_test, predictions))
-print(f'f1{i}',         f1_score(y_test, predictions))
-tn, fp, fn, tp = confusion_matrix(y_test, predictions).ravel()
-specificity = tn / (tn+fp)
-specificity
-print(f'Specificity1{i}',   specificity)
+    plt.figure(i*i)
+    plt.title('Loss')
+    plt.plot(metrics[['loss', 'val_loss']], label=[f'loss{i}', f'val_loss{i}'])
+    #plt.ylim(-0.1, 2)
+    plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    plt.savefig(f'output/loss{i}.png', bbox_inches='tight')#
 
+    plt.figure(i*i+1)
+    plt.title('Accuracy')
+    plt.plot(metrics[['accuracy', 'val_accuracy']], label=[f'acc{i}', f'val_acc{i}'])
+    #plt.ylim(0.4, 1.1)
+    plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    plt.savefig(f'output/acc{i}.png', bbox_inches='tight')##
 
-
-
-
-##############################################
-print('LTM model done')
-##############################################
-
-i = Input(shape=(512,512,1))
-##1 Single layers
-##Model->1
-x = Conv2D(filters=64, kernel_size=(5,5), activation='relu', padding='same')(i)
-x = MaxPool2D(pool_size=(2,2))(x)
-x = Conv2D(filters=32, kernel_size=(3,3), activation='relu', padding='same')(x)
-x = MaxPool2D(pool_size=(2,2))(x)
-x = BatchNormalization()(x)
-x = Flatten()(x)
-x = Dense(180, activation='relu')(x)
-#x = Dense(90, activation='relu')(x)
-x1 = Dense(1, activation='sigmoid')(x)
-model1 = Model(i, x1)
-
-roc = tf.keras.metrics.AUC(name='roc')
-model1.compile(loss="binary_crossentropy", optimizer= 'adam', metrics=['accuracy', roc])
-model1.fit(x=X_train3, y= y_train, validation_data=(X_test3, y_test) ,epochs=200, verbose=0, callbacks=[early_stop, reduce_lr])
-pred = model1.predict(X_test3)
-
-i=1
-predictions = np.round(pred)
-metrics = pd.DataFrame(model1.history.history)
-
-print(f'Accuracy{i}_0.07',   accuracy_score(y_test, predictions))
-print(f'precision{i}_0.07',  precision_score(y_test, predictions))
-print(f'recall{i}_0.07',     recall_score(y_test, predictions))
-print(f'f1{i}_0.07',         f1_score(y_test, predictions))
-tn, fp, fn, tp = confusion_matrix(y_test, predictions).ravel()
-specificity = tn / (tn+fp)
-print(f'Specificity1{i}_0.07',   specificity)
-
-#plt.figure(i*i)
-#plt.title('Loss')
-#plt.plot(metrics[['loss', 'val_loss']], label=[f'loss{i}', f'val_loss{i}'])
-#plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-##plt.ylim(-0.1, 2)
-#plt.savefig(f'output/loss_classification{i}.png', bbox_inches='tight')
-
-#plt.figure(i*i+1)
-#plt.title('Accuracy')
-#plt.plot(metrics[['accuracy', 'val_accuracy']], label=[f'acc{i}', f'val_acc{i}'])
-#plt.ylim(0.4, 1.1)
-#plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-#plt.savefig(f'output/acc{i}.png', bbox_inches='tight')##
-#
-#    plt.figure(15)
-#    plt.title("Receiver operating characteristic example")
-#    plt.plot([0, 1], [0, 1], linestyle='--', lw=2, color='r', label='Chance', alpha=.2)
-#    plt.plot(fpr, tpr, lw=2, alpha=0.3, label=(f'ROC{i}', roc_auc))
-#    plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-#    #plt.xlim(-0.05, 1.05)
-#    #plt.ylim(-0.05, 1.05)
-#    plt.savefig('output/auc.png', bbox_inches='tight')
-
-##1 Single layers
-i = Input(shape=(512,512,1))
-##1 Single layers
-##Model->1
-x = Conv2D(filters=64, kernel_size=(5,5), activation='relu', padding='same')(i)
-x = MaxPool2D(pool_size=(2,2))(x)
-x = Conv2D(filters=32, kernel_size=(3,3), activation='relu', padding='same')(x)
-x = MaxPool2D(pool_size=(2,2))(x)
-x = BatchNormalization()(x)
-x = Flatten()(x)
-x = Dense(180, activation='relu')(x)
-#x = Dense(90, activation='relu')(x)
-#x1 = Dense(1, activation=activation)(x)
-x2 = Dense(1, activation='linear')(x)
-#model1 = Model(i, x1)
-model1 = Model(i, x2)
-
-model1.compile(loss='mean_squared_error', optimizer='adam', metrics=['mean_absolute_error'])
-model1.fit(x= X_train3, y =y_train2, validation_data= (X_test3, y_test2), callbacks=[reduce_lr], epochs=200, verbose=0)
-pred2 = model1.predict(X_test3)
-mae = mean_absolute_error(y_test2, pred2)
-rmse = mean_squared_error(y_test2, pred2)
-print('MAE_0.06', mae)
-print('RMSE-0.06', rmse)
-print('y_test2>>>','', np.array(y_test2))
-print('pred2>>>','', np.array(pred2.ravel()))
-
-fig = plt.figure(4)
-plt.scatter(x=y_test2, y=pred2, edgecolors='k', color='g', alpha=0.7)
-plt.plot([0, 1], [0, 1], linestyle='--', lw=2, color='r', label='Chance', alpha=.8)
-plt.plot([0, 1], [0, 1], linestyle='--', lw=2, color='r', label='0%', alpha=.8)
-plt.plot([0.03, 1], [0, 0.97], 'g--', linewidth=0.8)
-plt.plot([0, 0.97], [0.03, 1],    'g--', linewidth=0.8, label='$\pm$ 3%')
-plt.xlim(0.85, 1.01)
-plt.ylim(0.85, 1.01)
-plt.ylabel('predicted')
-plt.xlabel('Measured')
-plt.title('Dose blended images for Portal Dosimetry - DBIP')
-plt.legend()
-plt.savefig('output/Plot_egression.png', bbox_inches='tight')
-i=1
-metrics2 = pd.DataFrame(model1.history.history)
-plt.figure(6)
-plt.title('Loss')
-plt.plot(metrics2[['mean_squared_error', 'val_mean_squared_error']], label=[f'loss{i}', f'val_loss{i}'])
-plt.ylim(-0.1, 2)
-plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-plt.savefig(f'output/loss_regression{i}.png', bbox_inches='tight')
+    plt.figure(15)
+    plt.title("Receiver operating characteristic example")
+    plt.plot([0, 1], [0, 1], linestyle='--', lw=2, color='r', label='Chance', alpha=.2)
+    plt.plot(fpr, tpr, lw=2, alpha=0.3, label=(f'ROC{i}', roc_auc))
+    plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    #plt.xlim(-0.05, 1.05)
+    #plt.ylim(-0.05, 1.05)
+    plt.savefig('output/auc.png', bbox_inches='tight')
+    
+    i = i+1
