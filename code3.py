8b926b1cbd520383f2a5a120c94d473060c270f5
pquinterome <33006816+pquinterome@users.noreply.github.com>
2022-06-07 11:17:04 -0400

    plres

--- code3.py
+++ code3.py
@@ -212,7 +212,7 @@ print(f'f1{i}',         f1_score(y_test, predictions))#
 print('now regression')
 
 model4.compile(loss='mean_squared_error', optimizer='adam', metrics=['mean_absolute_error'])
-model4.fit(x=[X_train1, X_train3], y= y_train2, validation_data= ([X_test1, X_test3], y_test2), callbacks=[reduce_lr], epochs=400, verbose=0)
+model4.fit(x=[X_train1, X_train3], y= y_train2, validation_data= ([X_test1, X_test3], y_test2), callbacks=[early_stop, reduce_lr], epochs=200, verbose=0)
 pred2 = model4.predict((X_test1, X_test3))
 mae = mean_absolute_error(y_test2, pred2)
 rmse = mean_squared_error(y_test2, pred2)
