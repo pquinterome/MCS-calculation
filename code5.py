e9029059920f1c01860fc9fb44cd15327524e543
pquinterome <33006816+pquinterome@users.noreply.github.com>
2022-10-04 10:14:26 +0100

    jansjas

--- code5.py
+++ code5.py
@@ -135,9 +135,9 @@ for i in range(10):
     res2 = res2/res2.max()
     plt.figure(figsize=(28,4))
     x = np.arange(0.0, len(res2), 1)
-    plt.plot(X_test2[0], alpha=1, linewidth=5.5, label='MUcp_profile')
+    plt.plot(X_test2[i], alpha=1, linewidth=5.5, label='MUcp_profile')
     plt.plot(res2)
-    plt.fill_between(x= x, y1= X_test2[0].ravel(), y2= res2, color='r', label='feature')
+    plt.fill_between(x= x, y1= X_test2[i].ravel(), y2= res2, color='r', label='feature')
     plt.legend()
     plt.savefig(f'output/Mcp_{i}.png', bbox_inches='tight')
 
