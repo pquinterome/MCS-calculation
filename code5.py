44d91c570f670aa1de30f3f8d303d99c4a83b7f9
pquinterome <33006816+pquinterome@users.noreply.github.com>
2022-10-04 10:51:32 +0100

    activation

--- code5.py
+++ code5.py
@@ -137,7 +137,7 @@ for i in range(10):
     x = np.arange(0.0, len(res2), 1)
     plt.plot(X_test2[i], alpha=1, linewidth=5.5, label='MUcp_profile')
     plt.plot(res2)
-    plt.fill_between(x= x, y1= X_test2[i].ravel(), y2= res2, color='gray', label='feature', alpha=0.5, where= res2>0.7)
+    plt.fill_between(x= x, y1= X_test2[i].ravel(), y2= res2, color='gray', label='Activation', alpha=0.5, where= res2>0.7)
     plt.legend()
     plt.savefig(f'output/Mcp_{i}.png', bbox_inches='tight')
 
